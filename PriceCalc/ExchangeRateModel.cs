116d6af6d09b4879f76fe45419d305e8606a2d68
kowgarnett <kowgarnett@gmail.com>
2017-04-03 23:42:00 -0400

    Add runtime serialization for future JSON use. There is a conflict betwen the JSON.Net library and VS for Mac as the JSON.Net depends on a .NET Runtime of version 4.0.20.0 which is not installable due to a constraint in NETStandardLibrary limiting the version >= 4.1.0. I've no idea who should I ask to commit this issue, but I will use the old way to work around. Async might be the problem but not a big one.

--- PriceCalc/ExchangeRateModel.cs
+++ PriceCalc/ExchangeRateModel.cs
@@ -1,16 +1,25 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace PriceCalc
 {
+    [DataContract]
     public class Rates
     {
+        [DataMember]
         public double CNY { get; set; }
     }
 
+    [DataContract]
     public class ExchangeRateObject
     {
+        [DataMember]
         public string @base { get; set; }
+
+        [DataMember]
         public string date { get; set; }
+
+        [DataMember]
         public Rates rates { get; set; }
     }
 }
